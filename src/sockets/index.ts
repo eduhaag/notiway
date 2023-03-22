e2465838bb270de3021c2e6c3270ae51351737a7
Eduardo Haag <eduardof.haag@gmail.com>
2023-03-22 15:11:45 -0300

    fix: update sender to paread when connection is ok

--- src/sockets/index.ts
+++ src/sockets/index.ts
@@ -2,18 +2,16 @@ import { prisma } from '@/lib/prisma'
 import socket from '@/lib/socket'
 
 export function sendersLogOnSocket() {
-  socket.off('session-logged').on('session-logged', async (status) => {
-    if (status.session === true) {
-      const sender = await prisma.sender.findUnique({
-        where: { name: status.session },
-      })
+  socket.off('session-logged').on('session-logged', async (data) => {
+    const sender = await prisma.sender.findUnique({
+      where: { name: data.session },
+    })
 
-      if (sender) {
-        await prisma.sender.update({
-          where: { id: sender.id },
-          data: { paread_at: new Date() },
-        })
-      }
+    if (sender) {
+      await prisma.sender.update({
+        where: { id: sender.id },
+        data: { paread_at: data.status ? new Date() : null },
+      })
     }
   })
 }

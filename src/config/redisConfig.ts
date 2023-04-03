89235043076db399a076f8260d1f2fa0a2a09da7
Eduardo Haag <eduardof.haag@gmail.com>
2023-04-03 16:55:49 -0300

    fix: remove redis-in-memory

--- src/config/redisConfig.ts
+++ src/config/redisConfig.ts
@@ -1,23 +1,22 @@
 import { env } from '@/env'
-import { RedisMemoryServer } from 'redis-memory-server'
 
-let redisServer
-let port
-let host
+// let redisServer
+const port = env.REDIS_PORT
+const host = env.REDIS_HOST
 
-if (env.NODE_ENV === 'test') {
-  redisServer = new RedisMemoryServer()
-  redisServer.getPort().then((data) => {
-    port = data
-  })
+// if (env.NODE_ENV === 'test') {
+//   redisServer = new RedisMemoryServer()
+//   redisServer.getPort().then((data) => {
+//     port = data
+//   })
 
-  redisServer.getHost().then((data) => {
-    host = data
-  })
-} else {
-  port = env.REDIS_PORT
-  host = env.REDIS_HOST
-}
+//   redisServer.getHost().then((data) => {
+//     host = data
+//   })
+// } else {
+//   port = env.REDIS_PORT
+//   host = env.REDIS_HOST
+// }
 
 export default {
   host,

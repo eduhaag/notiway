d3b3e3d8d17bbe3a8336a426137f2cb559bf61d0
Eduardo Haag <eduardof.haag@gmail.com>
2023-04-03 19:29:40 -0300

    fix: fix documentation path

--- src/fastify-register.ts
+++ src/fastify-register.ts
@@ -45,7 +45,9 @@ export async function fastiFyRegister(app: FastifyInstance) {
   app.register(sendersRoutes, { prefix: 'site' })
   app.register(messagesRoutes, { prefix: 'v1' })
 
-  const docs = readFileSync(path.resolve('public', 'docs.html'))
+  const docs = readFileSync(
+    path.resolve(__dirname, 'documentation', 'docs.html'),
+  )
 
   app.get('/docs', (req, reply) => {
     return reply.type('text/html').send(docs)
